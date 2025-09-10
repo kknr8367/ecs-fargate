965690048c0577061cdc4aeea06b1ccf401adbd7
Kamalanadha Reddy Kallutla <kamalanadhare.kallu@hcltech.com>
2025-09-10 16:23:09 +0530

    apply ecs code

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,3 +16,19 @@ resource "aws_subnet" "private" {
   vpc_id = aws_vpc.main.id
   cidr_block = cidrsubnet(var.cidr_block, 8, count.index + 2)
 }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
