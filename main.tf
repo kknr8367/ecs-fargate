27d8336623adb9a3daa00839fb268ef1e8a12065
Kamalanadha Reddy Kallutla <kamalanadhare.kallu@hcltech.com>
2025-09-10 16:59:11 +0530

    destroying, ECS fargate is working latest added the some code for application deployment, check and modify and work on that

--- main.tf
+++ main.tf
@@ -11,6 +11,10 @@ module "ecs" {
   private_subnets = module.vpc.private_subnets
   public_subnets  = module.vpc.public_subnets
   alb_arn         = module.alb.alb_arn
+  
+  task_definition = templatefile("${path.module}/task-definitions/patient-service.json", {
+  execution_role_arn = aws_iam_role.ecs_execution_role.arn
+})
 }
 
 module "alb" {
