ae312bc4679220885f19ebfdeae5533762464e33
Kamalanadha Reddy <143584583+kknr8367@users.noreply.github.com>
2025-10-16 10:53:48 +0000

    import testing

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -1,22 +1,21 @@
 terraform {
   required_version = ">= 1.6"
- 
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.0"
     }
   }
+
+  backend "s3" {
+    bucket  = "terraform-kamalb"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
 }
- 
+
 provider "aws" {
   region = var.aws_region
-}
-terraform {
-  backend "s3" {
-    bucket         = "terraform-kamalb"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"                   
-    encrypt        = true                          
-  }
 }
